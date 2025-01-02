dd2d95f19f86c29c0cb7d46f84775c9eb02362d8
Ikrommaulana <163699226+MaulanaDevtech422@users.noreply.github.com>
2025-01-02 21:59:52 +0700

    Update prom.sh

--- prom.sh
+++ prom.sh
@@ -1,23 +1,26 @@
+#!/bin/bash
+
 # Clear the terminal screen
-clear     
+clear
 red='\033[0;34m'
-green='\033[0;32m'                                                                     
+green='\033[0;32m'
 r="\033[0;35m"
-y="\033[0;34m"                             
+y="\033[0;34m"
 t="\033[0;33m"
-
-
-#code menampilkan tampilan promt keren
-./wi.sh
-figlet MaulanaDevtech | lolcat
-date | lolcat
+# Print the title using figlet
+echo ""
+#figlet bertujuan untuk memberikan efek teks style besar
+       ./wi.sh screenfetch -A Ubuntu
+   figlet -f slant "MaulanaDevtech" | lolcat
+echo  Web Devloper | lolcat
+./b.sh | lolcat
 # Print the bio description
-
+echo -e "${y}++++++++++++++++++++++++++++++++++++" | lolcat
+echo -e "${y}+            ${t}contact us           ${y} +" | lolcat
+echo -e "${y}++++++++++++++++++++++++++++++++++++" | lolcat
+echo -e "${y}+ ${t}Author    :Ikrom maulana         ${y}+" | lolcat
+echo -e "+ ${t}No Tel    :+6285712816240      ${y}  +" | lolcat
+echo -e "+ ${t}Instagram :imaulanaa_422    ${y}     +" | lolcat
+echo -e "+ ${t}Telegram  :@technocode422     ${y}   +" | lolcat
 echo -e "${y}++++++++++++++++++++++++++++++++++++"
-echo -e "${y}+            ${t}contact us           ${y} +"
-echo -e "${y}++++++++++++++++++++++++++++++++++++"
-echo -e "${y}+ ${t}Gmail     :technocode@gmail.com  ${y}+"
-echo -e "+ ${t}No Tel    :+6285712816240      ${y}  +"
-echo -e "+ ${t}Instagram :ikr.maulana422   ${y}     +"
-echo -e "+ ${t}Telegram  :@technocode422     ${y}   +"
-echo -e "${y}++++++++++++++++++++++++++++++++++++"
+echo "welcome to my command prompt :)"| lolcat
